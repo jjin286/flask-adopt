c762346aefc54d8bd26b64c106eac19bd233dd9f
Axa Pickering <axapickering@gmail.com>
2023-10-06 16:48:08 -0700

    ran in circles

--- models.py
+++ models.py
@@ -2,8 +2,20 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import CheckConstraint
-
+import requests
 db = SQLAlchemy()
+import os
+
+
+# this HAS to be imported if not in app.py
+from dotenv import load_dotenv
+load_dotenv()
+
+API_SECRET_KEY = os.environ['API_KEY']
+API_SECRET = os.environ['API_SECRET']
+API_TOKEN = os.environ['API_TOKEN']
+
+
 
 
 def connect_db(app):
@@ -44,8 +56,7 @@ class Pet(db.Model):
 
     notes = db.Column(
         db.Text,
-        nullable=False,
-        default='')
+        nullable=False)
 
     available = db.Column(
         db.Boolean,
@@ -53,4 +64,18 @@ class Pet(db.Model):
         default=True)
 
     __table_args__= dict(CheckConstraint(age in ['baby','young','adult','senior']))
+
+    @classmethod
+    def get_random_PetFinder_pet(cls):
+        ''' Requests PetFinder API to get a list of pets,
+            then randomly selects one '''
+
+        pets = requests.get('https://api.petfinder.com/v2/animals?limit=100',
+                            headers={"Authorization": f"Bearer {API_TOKEN}"})
+
+        # age = pets.animals[0].age
+        # name = pets.animals[0].name
+        # species = pets.animals[0].
+        # photo_url =
+
 
