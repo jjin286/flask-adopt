bedb7bac8afa41888b6b2f5a0a866cc7e8a013a2
Axa Pickering <axapickering@gmail.com>
2023-10-06 12:03:15 -0700

    Finished pet info page

--- forms.py
+++ forms.py
@@ -1,17 +1,29 @@
 """Forms for adopt app."""
-
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField
+from wtforms import StringField, SelectField, BooleanField
+from wtforms.validators import URL, Optional, InputRequired
 
 class AddPetForm():
     """Form for adding new pet"""
 
-    name = StringField("Pet name")
-    species = StringField("Species")
-    photo_url = StringField("Photo URL")
+    name = StringField("Pet name",validators=[InputRequired()])
+    species = SelectField('Species',
+        choices=[
+        ('cat','Cat'),
+        ('dog', 'Dog'),
+        ('porcupine', 'Porcupine')],validators=[InputRequired()])
+    photo_url = StringField("Photo URL",validators=[URL(),Optional()])
     age = SelectField('Age',
-        choices=[('baby','Baby'),
-                 ('young', 'Young'),
-                 ('adult', 'Adult')
-                 ('senior', 'Senior')])
-    notes = StringField("Notes")
+        choices=[
+            ('baby','Baby'),
+            ('young', 'Young'),
+            ('adult', 'Adult')
+            ('senior', 'Senior')],validators=[InputRequired()])
+    notes = StringField("Notes",validators=[Optional()])
+
+class EditPetForm():
+    """ Form for editing a pet """
+
+    photo_url = StringField("Photo URL",validators=[URL(),Optional()])
+    notes = StringField("Notes",validators=[Optional()])
+    available = BooleanField("Availability",default="checked",validators=[InputRequired()])
